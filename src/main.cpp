45f8cfe33c65d07e84f0c5b97f078f0159f5ab67
ishota <ishota1991@gmail.com>
2020-02-14 08:32:24 +0900

    bug fix

--- src/main.cpp
+++ src/main.cpp
@@ -126,7 +126,6 @@ int main() {
 
             weight_sum += particles[i].weight;
           }
-          std::cout << "num_particles " << num_particles << std::endl;
           std::cout << "highest w " << highest_weight << std::endl;
           std::cout << "average w " << weight_sum/num_particles << std::endl;
 
